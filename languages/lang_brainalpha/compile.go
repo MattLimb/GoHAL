b0e632c72b34fbfdcf2c37baf533a37e7cdab4b1
Matt Limb <matt.limb17@gmail.com>
2024-10-24 22:33:57 +0100

    Adding Brainalpha

--- languages/lang_brainalpha/compile.go
+++ languages/lang_brainalpha/compile.go
@@ -4,6 +4,6 @@ import (
 	"github.com/MattLimb/GoHAL/internal"
 )
 
-func compileAst(ast internal.Ast) (string, *internal.HalError) {
+func compileAst(_ internal.Ast) (string, *internal.HalError) {
 	return "", internal.NewCriticalHalError("brainalpha does not support transpilation", 0)
 }
